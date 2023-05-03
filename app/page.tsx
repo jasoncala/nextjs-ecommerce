c9a0bf4612a63df3578722c8deed79d7db9799dc
jasoncala <calaj@uwindsor.ca>
2023-05-03 15:37:12 -0400

    Added home page styling

--- app/page.tsx
+++ app/page.tsx
@@ -26,8 +26,10 @@ export default async function Home() {
   const products = await getProducts()
   console.log(products)
   return (
-    <main>
-      {products.map(product => <Product {...product}/>)}
+    <main className="grid grid-cols-fluid gap-12">
+      {products.map((product) => (
+        <Product {...product} key={product.id} />
+      ))}
     </main>
   )
 }
