0d6d761d3a49914487befa8f8e4d732c608feb68
jasoncala <calaj@uwindsor.ca>
2023-05-03 19:21:35 -0400

    More mobile friendly sizes

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -25,9 +25,9 @@ export default function Cart(){
             <motion.div 
                 layout
                 onClick={(e) => e.stopPropagation()} 
-                className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700">
+                className="bg-white absolute right-0 top-0 h-screen p-12 overflow-y-scroll text-gray-700 w-full lg:w-2/5">
                 
-                <h1>Here's your shopping list</h1>
+                <button onClick={() => cartStore.toggleCart()} className="text-sm font-bold pb-12">Back to store</button>
                 {cartStore.cart.map((item) => (
                     <motion.div layout key={item.id} className="flex py-4 gap-4">
                         <Image className="rounded-md h-24" src={item.image} alt={item.name} width={120} height={120}/>
